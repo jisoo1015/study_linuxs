df2f04b3ea4c35f0e9c0d6d72175f2ab3130bce9
jisoo1015 <kwltn34@gmail.com>
2025-07-14 18:07:42 +0900

    Create cd_pwd_ls.sh

---
+++ cods/codes/cd_pwd_ls.sh
@@ -0,0 +1,6 @@
+[kimjisoo@localhost ~]$ cd /home/kimjisoo/Music
+[kimjisoo@localhost Music]$ pwd
+/home/kimjisoo/Music
+[kimjisoo@localhost Music]$ ls
+[kimjisoo@localhost Music]$ ^C
+[kimjisoo@localhost Music]$ 
