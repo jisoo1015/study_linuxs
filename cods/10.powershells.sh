ddd3d7139bc91da10d0752d7b49062c0b91c0233
jisoo1015 <kwltn34@gmail.com>
2025-07-15 12:23:42 +0900

    두번째 올리기

---
+++ cods/10.powershells.sh
@@ -0,0 +1,19 @@
+
+PS C:\Users\Administrator> cd C:\
+PS C:\> cd C:/Develops
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14   오후 4:28                Downloads
